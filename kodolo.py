170b8a0b2165103f806ebc1271756065b78bb271
Lehoczki Patrícia <lehoczkipatricia99@gmail.com>
2020-10-14 10:53:21 +0200

    j

--- kodolo.py
+++ kodolo.py
@@ -14,10 +14,32 @@ def szovegKodol(szoveg):
 	szoveg.replace("í","ß")
 	szoveg.replace("ő","¤")
 	szoveg.replace("ő","=")
-	szoveg.replace("ü","=")
+	szoveg.replace("ü","@")
 	szoveg.replace("g","*")
 	szoveg.replace("c","+")
 	szoveg.replace("b",":")
 	return szoveg
+
+
+
+
+if (szam==2):
 	
-#szovegDeKodol():
+	text=str(input("Írd be a dekódolandó szöveget: "))
+"""
+	kesz=dekod.replace("$","a")
+	kesz=dekod.replace("Ł","é")
+	kesz=dekod.replace("ß","í")
+	kesz=dekod.replace("¤","ő")
+	kesz=dekod.replace("=","ő")
+	kesz=dekod.replace("@","ü")
+	kesz=dekod.replace("*","g")
+	kesz=dekod.replace("+","c")
+	kesz=dekod.replace(":","b")
+	print(kesz)
+	"""
+def szovegDeKodol(text):
+    chars = "\\`*_{}[]()>#+-.!$"
+    for c in chars:
+        text = text.replace(c, "\\" + c)
+		
