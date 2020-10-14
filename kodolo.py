51c6048b7fd7e61da4f54820bd7ead8b3c133c4d
AfRiyy <madaraszdavidd@gmail.com>
2020-10-14 10:14:31 +0200

    szovegKodol'

--- kodolo.py
+++ kodolo.py
@@ -1,7 +1,23 @@
 
+szam=int(input("Kódolás: 1, Dekódolás:2: "))
+if (szam==1):
+	szoveg=input("Írd be a szöveget:")	
+	szovegKodol(szoveg)
+	print(szoveg)
 
-szovegKodol():,
 
 
 
-szovegDeKodol():,
+def szovegKodol(szoveg):
+	szoveg.replace("a","$")
+	szoveg.replace("é","Ł")
+	szoveg.replace("í","ß")
+	szoveg.replace("ő","¤")
+	szoveg.replace("ő","=")
+	szoveg.replace("ü","=")
+	szoveg.replace("g","*")
+	szoveg.replace("c","+")
+	szoveg.replace("b",":")
+	return szoveg
+	
+#szovegDeKodol():
